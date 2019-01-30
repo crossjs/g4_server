dff7b4d31cae7fc22b8f6321a053b650c1b3b4df
crossjs <liwenfu@crossjs.com>
2019-01-30 21:28:52 +0800

    update

--- app/controller/pbl.ts
+++ app/controller/pbl.ts
@@ -3,6 +3,14 @@ import { Controller } from "egg";
 export default class PblController extends Controller {
   public async all() {
     const { ctx } = this;
-    ctx.body = await ctx.service.pbl.list({ enabled: true }, ["openId", "avatar", "nickname", "score"]);
+    ctx.body = await ctx.service.pbl.list(
+      {
+        enabled: true,
+        nickname: {
+          $ne: null,
+        },
+       },
+      ["openId", "avatar", "nickname", "score"],
+    );
   }
 }
